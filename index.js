6647d5baf33419939120417358e6881ef1e03af0
Murphyst <murphyst@Ms-MacBook-Pro.local>
2019-02-05 16:35:02 +0000

    mmc

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ let totalPriceOfTheBasket = 0;
 
 // Your functions will go below this line...
 
+function calculatePriceWithQuantity(a, b, c, d, e, f) {
+    return calculatePriceWithQuantity = (bagetPrice * bagetQuantity) + (butterPrice * butterQuantity) + (milkPrice * milkQuantity)
+}
 
-console.log( formatThePrice(totalPriceOfTheBasket) );
+var priceWithQuantity = calculatePriceWithQuantity(bagetPrice, bagetQuantity, butterPrice, butterQuantity, milkPrice, milkQuantity)
+
+function formatThePrice(calculatePriceWithQuantity) {
+    return 'The total price of your basket is: ' + '£' + calculatePriceWithQuantity.toFixed(2)
+}
+
+
+console.log( formatThePrice(priceWithQuantity) );
+
 
 // The output should be "The total price for your basket is: £5.82"
