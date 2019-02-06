59dae9db12def7946c27fb32625c55d66feb0f94
ahmet-serdar <nerelerdeyim505@gmail.com>
2019-02-06 18:26:54 +0100

    first-changes

--- index.js
+++ index.js
@@ -7,11 +7,31 @@ const butterQuantity = 1;
 const milkPrice = 0.86;
 const milkQuantity = 2;
 
-let totalPriceOfTheBasket = 0;
+
 
 // Your functions will go below this line...
 
+ function calculatePriceWithQuantity(price,quantity){
+     let priceWithQuantity = price*quantity;
+     return priceWithQuantity;
 
-console.log( formatThePrice(totalPriceOfTheBasket) );
+    }
+  let bagetPriceWithQuantity = calculatePriceWithQuantity(bagetPrice,bagetQuantity);
+  let butterPriceWithQuantity = calculatePriceWithQuantity(butterPrice,butterQuantity);
+  let milkPriceWithQuantity = calculatePriceWithQuantity(milkPrice,milkQuantity);  
+
+
+ function calculateTotalPriceOfTheBasket(item1,item2,item3){
+     let totalPriceOfTheBasket =  item1+item2+item3;
+    return totalPriceOfTheBasket;
+ }
+    let totalPriceOfTheBasket = calculateTotalPriceOfTheBasket(bagetPriceWithQuantity,butterPriceWithQuantity,milkPriceWithQuantity);
+
+ function formatThePrice(price){
+   let formatPrice = price.toFixed(2)
+   return formatPrice;
+ }
+
+    console.log( formatThePrice(totalPriceOfTheBasket) );
 
 // The output should be "The total price for your basket is: £5.82"
